a7584e41404a00a7f0753a871fbeea60434c7e03
wttch <54383426+wttch96@users.noreply.github.com>
2021-11-12 18:08:06 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,6 @@
 rootProject.name = "wttch-wcbs"
 include("wcbs-core")
 include("wcbs-spring-boot-starter")
-include("wcbs-spring-boot-autoconfigure")
 include("wcbs-test")
 include("wcbs-web")
 include("wcbs-data-jdbc")
